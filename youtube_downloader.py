9e62b1b0d700045b05a3eb4a8c91a0ec57c7be8c
Kasper Schmidt <94352891+Kasper-Schmidt@users.noreply.github.com>
2026-02-13 14:14:14 +0100

    extra

---
+++ youtube_downloader.py
@@ -0,0 +1,44 @@
+from pytubefix import YouTube
+import tkinter as tk
+from tkinter import filedialog
+
+
+def download_video(url, save_path):
+    try:
+        yt = YouTube(url)
+        streams = yt.streams.filter(progressive=True, file_extension="mp4")
+        highest_res_stream = streams.get_highest_resolution()
+        highest_res_stream.download(output_path=save_path)
+        print("Video Downloaded Successfully")
+    except Exception as e:
+        print(e)
+
+
+def open_file_dialog():
+    folder = filedialog.askdirectory()
+    if folder:
+        print(f"Selected folder: {folder}")
+
+    return folder
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.withdraw()
+
+    video_url = input("Enter YouTube url: ")
+    save_dir = open_file_dialog()
+
+    if not save_dir:
+        print("Invalid save location")
+    else:
+        download_video(video_url, save_dir)
+
+
+
+
+
+url = "https://www.youtube.com/watch?v=mRFsQgs1TmQ"
+save_path = "C:/Users/kas/Desktop"
+
+download_video(url, save_path)
